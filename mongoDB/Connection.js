b98dd6724b65b87ba83ac487c86f80a0a0f6347a
Anil Bhandari <93316453+onealanil@users.noreply.github.com>
2023-07-15 22:53:07 +0545

    55

--- mongoDB/Connection.js
+++ mongoDB/Connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const url = "mongodb+srv://Onealanil:helloworld123@cluster0.equk5.mongodb.net/messengerMERN?retryWrites=true&w=majority";
+const url = "mongodb+srv://@cluster0.equk5.mongodb.net/messengerMERN?retryWrites=true&w=majority";
 mongoose
   .connect(url, {
     useUnifiedTopology: true,
